bccabc8a314bbc6b85e43e409dc74ddb8cba3cf4
HernanGoldy <hernanmdq@gmail.com>
2022-01-20 13:27:33 -0300

    Modifico ruta de la view contacto

--- ProyectoWeb/contacto/views.py
+++ ProyectoWeb/contacto/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def contacto(request):
-    return render(request, "ProyectoWebApp/contacto.html")
+    return render(request, "contacto/contacto.html")
