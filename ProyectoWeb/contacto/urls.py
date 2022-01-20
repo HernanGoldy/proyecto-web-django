c00c5a99c6986cfccf8a53bd40913df53cad7347
HernanGoldy <hernanmdq@gmail.com>
2022-01-20 13:24:45 -0300

    Configuro urls de la app contacto

---
+++ ProyectoWeb/contacto/urls.py
@@ -0,0 +1,9 @@
+# contacto URL configuration
+
+from xml.dom.minidom import Document
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.contacto, name="Contacto"),
+]
