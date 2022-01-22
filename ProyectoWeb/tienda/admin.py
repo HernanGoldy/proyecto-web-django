d58976d470972ad9e2ba74d603e89a1c85547752
HernanGoldy <hernanmdq@gmail.com>
2022-01-22 10:05:25 -0300

    Registro en Django admin las cat. de solo lectura

--- ProyectoWeb/tienda/admin.py
+++ ProyectoWeb/tienda/admin.py
@@ -1,3 +1,12 @@
+from pydoc import classname
 from django.contrib import admin
+from .models import CategoriaProd, Producto
 
 # Register your models here.
+
+# Le indicamos a Django que los campos created y updated son de solo lectura
+class CategoriaProdAdmin(admin.ModelAdmin):
+    readonly_fields= "created", "updated"
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields= "created", "updated"
